9ed0ad7fc02f7a727e48fc48142f829e172cc931
l00624481 <longyun13@huawei.com>
2022-02-18 17:12:14 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro_derive(IntoResponse)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_into_response_macro(&ast)
+}
+
+fn impl_into_response_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl axum::response::IntoResponse for #name {
+            fn into_response(self) -> axum::response::Response {
+                let bytes = match serde_json::to_vec(&self) {
+                    Ok(res) => res,
+                    Err(err) => {
+                        return axum::response::Response::builder()
+                            .status(hyper::StatusCode::INTERNAL_SERVER_ERROR)
+                            .header(
+                                hyper::header::CONTENT_TYPE,
+                                axum::http::HeaderValue::from_static(mime::TEXT_PLAIN_UTF_8.as_ref()),
+                            )
+                            .body(axum::body::boxed(axum::body::Full::from(err.to_string())))
+                            .unwrap();
+                    }
+                };
+                let mut res =
+                    axum::response::Response::new(axum::body::boxed(axum::body::Full::from(bytes)));
+                res.headers_mut().insert(
+                    hyper::header::CONTENT_TYPE,
+                    axum::http::HeaderValue::from_static(mime::APPLICATION_JSON.as_ref()),
+                );
+                res
+            }
+        }
+    };
+    gen.into()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
